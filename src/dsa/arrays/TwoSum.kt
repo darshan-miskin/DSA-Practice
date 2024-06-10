872337fb4ba88328310e40cb04b49f92794a0f65
Darshan Miskin <28859845+darshan-miskin@users.noreply.github.com>
2024-06-10 11:20:51 +0530

    two sum

---
+++ src/dsa/arrays/TwoSum.kt
@@ -0,0 +1,37 @@
+package dsa.arrays
+
+/**
+ * Takes in two params. The function checks if two numbers from [nums] can be added
+ * to get the [target]
+ * @param nums Integer array
+ * @param target sum value
+ *
+ * @return solution array containing indexes, returns an empty array if no solution.
+ */
+fun twoSum(nums: IntArray, target: Int): IntArray {
+    val map = HashMap<Int, Int>()
+    val array = ArrayList<Int>()
+    for((index, num) in nums.withIndex()){
+        if(map.contains(num)){
+            array.add(map.getValue(num))
+            array.add(index)
+        }
+        map[(target - num)] = index
+    }
+    return array.toIntArray()
+}
+
+fun main() {
+    var solution=twoSum(arrayOf(3,7,11,15).toIntArray(), 9)
+    println(solution.toList().toString())
+
+    solution = twoSum(arrayOf(3,2,4).toIntArray(), 6)
+    println(solution.toList().toString())
+}
+
+fun toSeconds(time: String): (Int) -> Int = when (time) {
+    "hour" -> { value1 -> value1 * 60 * 60 }
+    "minute" -> { value -> value * 60 }
+    "second" -> { value -> value }
+    else -> { value -> value }
+}
